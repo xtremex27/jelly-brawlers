1f311f63285b933d8fbb7006a77b4686787c5de6
xtremex27 <xtremex27@users.noreply.github.com>
2026-02-02 22:09:25 -0500

    Fix: Listen on process.env.PORT for Render

--- server.js
+++ server.js
@@ -86,6 +86,7 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
 });
